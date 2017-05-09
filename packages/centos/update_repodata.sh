c0003aefb1e242daad96d6ca02e407bd5eb0a970
ss75710541 <75710541@qq.com>
2017-05-09 17:17:01 +0800

    @yhchen centos7.3 兼容性

--- packages/centos/update_repodata.sh
+++ packages/centos/update_repodata.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 base_dir=$(cd `dirname $0` && pwd)
 cd $base_dir
+version=$1
 
-createrepo --update 7/x86_64/ 
+if [ -z "$version" ];then
+	echo "Usage: ./$0 version,version value is 7.2 or 7.3"
+	exit 1
+fi
+createrepo --update $version/x86_64/ 
--- run.sh
+++ run.sh
@@ -9,12 +9,12 @@ chmod +x /etc/rc.local
 ps aux|grep "ForkStaticServer"|grep -v grep|wc -l|grep 1 || nohup ./ForkStaticServer.py $CONFIGSERVER_LOCAL_IP:$CONFIGSERVER_PORT 1>/tmp/sry_yumrepo.log 2>&1 &
 
 sleep 2
-curl -Ls http://$CONFIGSERVER_IP:$CONFIGSERVER_PORT/packages/centos/get_repo.sh|bash -s $CONFIGSERVER_IP:$CONFIGSERVER_PORT
+curl -Ls http://$CONFIGSERVER_IP:$CONFIGSERVER_PORT/packages/centos/get_repo.sh|bash -s $CONFIGSERVER_IP:$CONFIGSERVER_PORT $CENTOS_VERSION
 
 rm -f yum_repo_readme.txt
 echo "please look yum_repo_readme.txt"
 echo "install repo command:"|tee -a yum_repo_readme.txt
-echo "curl -Ls http://$CONFIGSERVER_IP:$CONFIGSERVER_PORT/packages/centos/get_repo.sh|bash -s $CONFIGSERVER_IP:$CONFIGSERVER_PORT"|tee -a yum_repo_readme.txt
+echo "curl -Ls http://$CONFIGSERVER_IP:$CONFIGSERVER_PORT/packages/centos/get_repo.sh|bash -s $CONFIGSERVER_IP:$CONFIGSERVER_PORT $CENTOS_VERSION"|tee -a yum_repo_readme.txt
 echo "" | tee -a yum_repo_readme.txt 
 echo "yum install command:" | tee -a yum_repo_readme.txt
 echo "yum --disablerepo=\* --enablerepo=offlineshurenyunrepo install -y <PACKAGE_NAME>" | tee -a yum_repo_readme.txt
