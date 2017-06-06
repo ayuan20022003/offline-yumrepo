d3b572acac4c449fc4f06a7d7eee13838cdd8af4
root <root@srymaster1.node.dataman.consul>
2017-06-06 11:54:49 +0800

    rename 7.2ee to docker-ee

--- packages/centos/update_repodata.sh
+++ packages/centos/update_repodata.sh
@@ -4,7 +4,7 @@ cd $base_dir
 version=$1
 
 if [ -z "$version" ];then
-	echo "Usage: ./$0 version,version value is 7.2|7.3|7.2ee|7.3ee"
+	echo "Usage: ./$0 version,version value is 7.2|7.3|docker-ee"
 	exit 1
 fi
 createrepo --update $version/x86_64/ 
