28dbcf8842bf0e6184be8c55ba918a0dca06f913
root <75710541@qq.com>
2017-12-06 20:42:56 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ fi
 
 grep offline-yumrepo /etc/rc.local || echo "/bin/bash /data/offline-yumrepo/run.sh" >> /etc/rc.local
 chmod +x /etc/rc.local
-ps aux|grep "ForkStaticServer"|grep -v grep|wc -l|grep 1 || nohup ./ForkStaticServer.py $CONFIGSERVER_LOCAL_IP:$CONFIGSERVER_PORT 1>/tmp/sry_yumrepo.log 2>&1 &
+ps aux|grep "ForkStaticServer"|grep -v grep|wc -l|grep 1 || nohup ./ForkStaticServer.py $CONFIGSERVER_IP:$CONFIGSERVER_PORT 1>/tmp/sry_yumrepo.log 2>&1 &
 
 sleep 2
 
