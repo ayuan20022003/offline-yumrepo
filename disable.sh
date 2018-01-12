5717ea5228efa6848d8e683aea9f6552e173bca9
ss75710541 <75710541@qq.com>
2018-01-12 19:19:51 +0800

    update enable.sh disable.sh

--- disable.sh
+++ disable.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ex
+base_dir=$(cd `dirname $0` && pwd)
+cd $base_dir
 
 systemctl disable offline-yumrepo
 systemctl stop offline-yumrepo
--- enable.sh
+++ enable.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ex
+base_dir=$(cd `dirname $0` && pwd)
+cd $base_dir
 
 cp offline-yumrepo.service /usr/lib/systemd/system/
 systemctl enable offline-yumrepo
