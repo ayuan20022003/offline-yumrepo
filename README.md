4a551dc1d9b54816ac643a4cf1ede624e583dd8d
chanehua <835870393@qq.com>
2018-02-22 14:31:59 +0800

    @yhchen add load config.cfg path

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 
 ## 安装方法：
 
-修改../offlinesry/config.cfg 或 ../config.cfg 或 ./config.cfg中
+修改../offlinesry/config.cfg 或 . ../offline-dmos/config.cfg  或../config.cfg 或 ./config.cfg中
 
 ```
 CONFIGSERVER_IP="192.168.1.214"    ## Need to check
--- check.sh
+++ check.sh
@@ -3,6 +3,14 @@ set -e
 base_dir=$(cd `dirname $0` && pwd)
 cd $base_dir
 
-. ../offlinesry/config.cfg
+if [  -f "../offlinesry/config.cfg" ];then
+        . ../offlinesry/config.cfg
+elif [ -f "../offline-dmos/config.cfg" ]; then
+        . ../offline-dmos/config.cfg
+elif [ -f "../config.cfg" ];then
+        . ../config.cfg
+else
+        . ./config.cfg
+fi
 
 curl -s -o /dev/null -w "%{http_code}" --connect-timeout 10 http://$CONFIGSERVER_IP:$CONFIGSERVER_PORT/|grep 200
